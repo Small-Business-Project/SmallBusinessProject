2fbca1598d889a850f66f4ff83de775ebef616c6
Yousif <yousufalkhateeb@gmail.com>
2022-04-24 17:51:47 -0600

    Undo some comit changes of previous commit

--- models/User.js
+++ models/User.js
@@ -13,6 +13,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    appointments: {
+        day : {type: Date},
+        time: {type: Number}
+    }
 },
 );
 
